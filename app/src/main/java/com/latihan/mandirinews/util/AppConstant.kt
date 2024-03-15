4b9818768e45e2f5c9444a88f283949389cbf4fb
Yazid Bintang <yazidbintang2711@gmail.com>
2024-03-16 01:06:42 +0700

    Change Q

--- app/src/main/java/com/latihan/mandirinews/util/AppConstant.kt
+++ app/src/main/java/com/latihan/mandirinews/util/AppConstant.kt
@@ -4,4 +4,5 @@ object AppConstant {
     const val API_KEY = "e6ec895d7fe64a90afd876c5c474f691"
     const val BASE_URL = "https://newsapi.org/v2/"
     const val Country = "US"
+    const val Q = "google"
 }
